877206df057c1f3eaa0da6c763bc55086f0121a8
AnshBhardwaj <anshbhardwaj119@gmail.com>
2024-03-04 01:43:58 +0530

    inital commit

--- index.py
+++ index.py
@@ -1,11 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/home')
-def index():
-    return ("Hello World!")
-
-@app.route('/about')
-def about():
-    return ("About Page")
+from wsgi import app
