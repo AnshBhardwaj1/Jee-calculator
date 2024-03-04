65e777ea426d41aa481257ab95ec273791cbe5db
AnshBhardwaj <anshbhardwaj119@gmail.com>
2024-03-04 09:28:59 +0530

    both .html and link way are working now.

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from mains_marks import give_marks  # Import the function from your script
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = '/tmp'
 
 @app.route('/')
 def index():
-    return ("Hello World!")
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return ("About Page")
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    if 'html' not in request.files:
+        return 'No file part'
+    file = request.files['html']
+    if file.filename == '':
+        return 'No selected file'
+    if file:
+        filename = secure_filename(file.filename)
+        directory = os.path.join(app.config['UPLOAD_FOLDER'], "Test.html")
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        file_path = os.path.join(directory, filename)
+        file.save(file_path)
+        
+        output = give_marks(file_path)
+        
+        return render_template('result.html', output=output)
+
+@app.route('/upload_url', methods=['POST'])
+def upload_url():
+    url = request.form.get('url')
+    if not url:
+        return 'No URL provided'
+    
+    output = give_marks(url)
+    
+    return render_template('result.html', output=output)
---
+++ mains_marks.py
@@ -0,0 +1,155 @@
+from keys_data import *
+class question :
+    def __init__(self,questionNumber, questionID, answerID, KeyID):
+        self.questionNumber = questionNumber
+        self.questionID = questionID
+        self.answerID = answerID
+        self.KeyID = KeyID
+    def marks(self):
+        if self.answerID != ' -- ':
+            if self.answerID == self.KeyID and self.answerID != "Drop":
+                return 4
+            if self.answerID != self.KeyID  and self.KeyID != " -- " :
+                return -1
+        else:
+            return 0 
+    def __str__(self):
+        return "Question Number: " + self.questionNumber + " Question ID: " + self.questionID + " Answer ID: " + self.answerID + " Key ID: " + self.KeyID
+
+def soup_from_link(link):
+    import requests
+    from bs4 import BeautifulSoup
+    page=requests.get(link)
+    soup=BeautifulSoup(page.content,'lxml')
+    return soup
+def soup_from_file(file):
+    from bs4 import BeautifulSoup
+    with open(file, 'r') as file:
+        soup=BeautifulSoup(file,'lxml')
+    return soup
+def give_marks_of_soup(soup):
+    option_chose=None
+    marked={}
+    marks=0
+    tag=list(soup.find_all('td'))
+    tag = list(map(lambda i: i.text, tag))
+    for i in range(len(tag)):
+        if "Test Date" in tag[i]:
+            date=(tag[i+1])
+        if "Test Time" in tag[i]:
+            time=(tag[i+1])
+    if date=="27/01/2024" and time=="9:00 AM - 12:00 PM":
+        key=mains27janshift1anskey
+    if date=="27/01/2024" and time=="3:00 PM - 6:00 PM":
+        key=mains27janshift2anskey
+    if date=="29/01/2024" and time=="9:00 AM - 12:00 PM":
+        key=mains29janshift1anskey
+    if date=="29/01/2024" and time=="3:00 PM - 6:00 PM":
+        key=mains29janshift2anskey
+    if date=="30/01/2024" and time=="9:00 AM - 12:00 PM":
+        key=mains30janshift1anskey
+    if date=="30/01/2024" and time=="3:00 PM - 6:00 PM":
+        key=mains30janshift2anskey
+    if date=="31/01/2024" and time=="9:00 AM - 12:00 PM":
+        key=mains31janshift1anskey
+    if date=="31/01/2024" and time=="3:00 PM - 6:00 PM":
+        key=mains31janshift2anskey
+    if date=="01/02/2024" and time=="9:00 AM - 12:00 PM":
+        key=mains1febshift1anskey
+    if date=="01/02/2024" and time=="3:00 PM - 6:00 PM":
+        key=mains1febshift2anskey
+
+    #option_chose=None
+    questions=[]
+    question_ID=None
+    for i in range(len(tag)):
+        #questionID=4058591108
+        if "Question ID :" == tag[i]: 
+            #print("Question ID is ",tag[i+1])
+            question_ID=tag[i+1]
+
+        # to get answers of mcq type
+        if "Option 1 ID :" == tag[i]: 
+            opt1_ID=tag[i+1]
+            #print ("Option 1 ID is ",tag[i+1])
+        if "Option 2 ID :" == tag[i]:
+            opt2_ID=tag[i+1]
+            #print ("Option 2 ID is ",tag[i+1])
+        if "Option 3 ID :" == tag[i]:
+            opt3_ID=tag[i+1]
+            #print ("Option 3 ID is ",tag[i+1])
+        if "Option 4 ID :" == tag[i]:
+            opt4_ID=tag[i+1]
+            #print ("Option 4 ID is ",tag[i+1])
+        if "Chosen Option :" == tag[i]:
+            option_chose=tag[i+1]
+            #print("Chosen Option is ",tag[i+1])
+            if option_chose != " -- ":
+                if option_chose=="1":
+                    answer_ID=opt1_ID
+                if option_chose=="2":
+                    answer_ID=opt2_ID
+                if option_chose=="3":
+                    answer_ID=opt3_ID
+                if option_chose=="4":
+                    answer_ID=opt4_ID
+            else:
+                answer_ID=" -- "
+
+        #to get answers of numerical type
+        if "Given Answer :" == tag[i]: 
+            given_ans=tag[i+1]
+            answer_ID=given_ans
+            #print("Given Answer is ",tag[i+1])
+
+        #to get question number
+        if "Q." in tag[i]:
+            if len (tag[i])<6:
+                questionNumber=tag[i][2:]
+                if question_ID is not None: 
+                    key_ID=key[question_ID]
+                    questions.append(question(questionNumber, question_ID, answer_ID, key_ID))
+                    #print (questionNumber, question_ID, answer_ID, key_ID)
+    
+
+    maths=0
+    for i in range(1,32):
+        maths = maths + questions[i].marks()
+    physics=0
+    for i in range(32,62):
+        physics = physics + questions[i].marks()
+    chemistry=questions[0].marks()
+    for i in range(62,len(questions)):
+        chemistry = chemistry + questions[i].marks()
+    total=maths+physics+chemistry
+    print("Maths:",maths)
+    print("Physics:",physics)
+    print("Chemistry:",chemistry)
+    print("Total:",total)
+    return (total)
+import os
+from pathlib import Path
+
+def give_marks(whatever):
+    if whatever.startswith("http"):
+        soup = soup_from_link(whatever)
+        marks = give_marks_of_soup(soup)
+    else:
+        print(f"Checking if file {whatever} exists...")
+        if Path(whatever).is_file():
+            print(f"File {whatever} exists. Proceeding...")
+            soup = soup_from_file(whatever)
+            marks = give_marks_of_soup(soup)
+            os.remove(whatever) 
+        else:
+            print(f"File {whatever} does not exist.")
+            return
+    return marks
+    
+if __name__=="__main__":
+    link=("https://cdn3.digialm.com//per/g28/pub/2083/touchstone/AssessmentQPHTMLMode1//2083O23354/2083O23354S15D16244/17067996516627886/KK14005904_2083O23354S15D16244E1.html#")
+    marks=give_marks(link)
+    print (f"Your marks are {marks}")
+    marks_html=give_marks('Test.html')
+    print (f"Your marks are {marks_html}")
+        
